c349151211071de47b252b50ac267b42d5560b2b
UO258740 <UO258740@uniovi.es>
2022-10-07 16:28:22 +0200

    Se enumeran las reglas que se han roto en ejemplo1

--- ejemplo1.c
+++ ejemplo1.c
@@ -12,6 +12,15 @@
  *     selected (you can remove/change instructions).
  */
 
+/**
+ * REGLAS ROTAS
+ * ARR01-C (linea 70): No se aplica sizeof() para calcular la longitud del array
+ * MSC34-C (lineas 40, 88, 106, 107): Se utilizan funciones deprecadas
+ * STR31-C (lineas 79-80): No se garantiza el almacenamiento necesario para los arrays
+ * STR32-C (linea 81): No se reserva espacio para para el caracter /0
+ * STR35-C (lineas 88): Se copia informacion de la variable key, que esta vacia
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -30,7 +39,7 @@ const char* s2 = "\nHello\nWorld\n";
 void gets_example_func(void) {
   char buf[BUFFER_MAX_SIZE];
  
-  if (fgets(buf, sizeof(buf), stdin) == NULL) { // fgets es una funcion deprecada
+  if (fgets(buf, sizeof(buf), stdin) == NULL) { // WARNING: fgets es una funcion deprecada
         return 1; // ERROR: Una funcion vacia no puede retornar un valor
   }
   buf[strlen(buf) - 1] = '\0';
@@ -67,17 +76,17 @@ int main(int argc, char *argv[])
     char array4[16];
     char array5 []  = "01234567890123456";
     char *ptr_char  = "new string literal"; // WARNING: Las versiones C++11 o mas antiguas no admiten la conversion de String literal a char *
-    int size_array1 = strlen("аналитик");
+    int size_array1 = strlen("аналитик"); 
     int size_array2 = 100;
     
-   // char analitic1[size_array1]="аналитик";
-   // char analitic2[size_array2]="аналитик";
+   // char analitic1[size_array1]="аналитик"; // ERROR: String no inicializada
+   // char analitic2[size_array2]="аналитик"; // ERROR: String no inicializada
     char analitic3[100]="аналитик";
 
     puts(get_dirname(__FILE__));
 
         
-    strcpy(key, argv[1]);  
+    strcpy(key, argv[1]); // WARNING: Funcion deprecada
     strcat(key, " = ");  
     strcat(key, argv[2]);
 
@@ -96,8 +105,8 @@ int main(int argc, char *argv[])
     puts (s2);
     printf ("\n");
     
-    strncpy(array3, array5, sizeof(array3));  
-    strncpy(array4, array3, strlen(array3));
+    strncpy(array3, array5, sizeof(array3)); // WARNING: Funcion deprecada
+    strncpy(array4, array3, strlen(array3)); // WARNING: Funcion deprecada
     
     array5 [0] = 'M';
     ptr_char [0] = 'N';
